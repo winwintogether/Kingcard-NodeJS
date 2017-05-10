7c918f880772e99c2e8b42ccc3b8ec0a438259ef
winwintogether <masterinbusiness@outlook.com>
2017-05-11 01:29:08 +0200

    Update code

--- App_Data/config/scripts/api/getAllUsers.js
+++ App_Data/config/scripts/api/getAllUsers.js
@@ -1,19 +1,5 @@
 
-        return;
-    }
-    var errorHandler = function (err) {
-        console.error(err);
-        response.send(statusCodes.INTERNAL_SERVER_ERROR, err);
-    };
-            
-            
-            var bbGapi = require('../shared/bbGraphAPI.js');
-
-            bbGapi.getAADToken(function (err, access_token) {
-                if (err) errorHandler(err);
-                else{
-                    console.log("getAllUsers: access_token=" + access_token);
-                    
+   
                     bbGapi.getAllUsers(access_token, function (err, user_info) {
                         if (err) errorHandler(err);
                         else{
